0f55d344a0c8aeb2f28e1159cc3bbff7e87b891b
Ladya2003 <vladiator_one@mail.ru>
2022-12-29 16:07:10 +0300

    yes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let a = 91, b = 121, i = 1;
 
 function time90s() {
+    // b=-1;
     let timer = document.querySelector("#timer");
     let def_text = document.querySelector(".timer-default-text");
     if (a >= i) {
@@ -8,6 +9,9 @@ function time90s() {
         def_text.innerHTML = "";
         timer.innerHTML = a;
         setTimeout(time90s, 1000);
+    }
+    else if (a==-1) {
+        
     }
     else {
         alert("Time is up! To start new timer, choose new time.");
@@ -17,6 +21,7 @@ function time90s() {
     }
 }
 function time120s() {
+    // a=-1;
     let timer = document.querySelector("#timer");
     let def_text = document.querySelector(".timer-default-text");
     if (b >= i) {
@@ -24,6 +29,9 @@ function time120s() {
         def_text.innerHTML = "";
         timer.innerHTML = b;
         setTimeout(time120s, 1000);
+    }
+    else if (b==-1) {
+
     }
     else {
         alert("Time is up! To start new timer, choose new time.");
@@ -33,17 +41,34 @@ function time120s() {
     }
 }
 
+function resetAll() {
+    a=-1;
+    b=-1;
+
+    setTimeout(a=91, 10);
+    setTimeout(a=121, 10);
+}
+
 function progressBar90s() {
     let progress_bar = document.querySelector("#progress-bar");
     let progress = `<div class="progress90s"></div>`;
     progress_bar.innerHTML = progress;
     setTimeout(audio, 90500);
+
+    let timer = document.querySelector(".timer");
+    timer.classList.remove("timer-120s-anim");
+    timer.classList.add("timer-90s-anim");
+    // setTimeout(timer.classList.remove("timer-90s-anim"))
 }
 function progressBar120s() {
     let progress_bar = document.querySelector("#progress-bar");
     let progress = `<div class="progress120s"></div>`;
     progress_bar.innerHTML = progress;
     setTimeout(audio, 119000);
+
+    let timer = document.querySelector(".timer");
+    timer.classList.remove("timer-90s-anim");
+    timer.classList.add("timer-120s-anim");
 }
 
 function audio() {
